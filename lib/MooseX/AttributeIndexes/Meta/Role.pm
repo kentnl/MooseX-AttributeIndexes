7cdf6b910df9e0c34bc81d69400bfe8c44053914
Kent Fredric <kentfredric@gmail.com>
2011-04-04 19:39:47 +1200

    basic strictures for critic bs

--- lib/MooseX/AttributeIndexes/Meta/Role.pm
+++ lib/MooseX/AttributeIndexes/Meta/Role.pm
@@ -1,5 +1,17 @@
+use strict;
+use warnings;
+
 package MooseX::AttributeIndexes::Meta::Role;
+
+# ABSTRACT: MetaRole for AttributeIndexes.
+
 use Moose::Role;
+
+=method composition_class_roles
+
+returns C<MooseX::AttributeIndexes::Meta::Role::Composite>
+
+=cut
 
 sub composition_class_roles {
     'MooseX::AttributeIndexes::Meta::Role::Composite'
--- lib/MooseX/AttributeIndexes/Meta/Role/ApplicationToRole.pm
+++ lib/MooseX/AttributeIndexes/Meta/Role/ApplicationToRole.pm
@@ -1,3 +1,6 @@
+use strict;
+use warnings;
+
 package MooseX::AttributeIndexes::Meta::Role::ApplicationToRole;
 use Moose::Role;
 
