ea8330b6842e69922b367e76f1ab2c382cad2c1b
Kent Fredric <kentfredric@gmail.com>
2014-09-24 17:04:55 +1200

    Build results of 3280e23 (on master)

--- lib/MooseX/AttributeIndexes.pm
+++ lib/MooseX/AttributeIndexes.pm
@@ -202,7 +202,7 @@ don't behave like they should.
 
 =head3 SEE ALSO
 
-L<< Search::GIN::Extract::AttributeIndexes >>
+L<< C<Search::GIN::Extract::AttributeIndexes>|Search::GIN::Extract::AttributeIndexes >>
 
 =head1 AUTHORS
 
