9261fd885cd9470189f8c59c04d138b29540499b
Kent Fredric <kentfredric@gmail.com>
2017-03-09 05:20:04 +1300

    Build results of 4569d1f (on master)

--- lib/MooseX/AttributeIndexes/Provider/FromAttributes.pm
+++ lib/MooseX/AttributeIndexes/Provider/FromAttributes.pm
@@ -102,7 +102,7 @@ Jesse Luehrs <doy@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2014 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
